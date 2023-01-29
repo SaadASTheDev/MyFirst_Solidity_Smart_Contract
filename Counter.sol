1e15440216a8120bd4dbcc3e13849758029f9da2
Saad Ahmad Sabri <105510022+SaadASTheDev@users.noreply.github.com>
2023-01-28 21:48:34 -0500

    My first Set and Get solidity File Added counter smart contract

---
+++ Counter.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+    //  Unsigned Integer (uint) means an integer without a sign in from of it for example -1
+    uint count;
+
+
+    constructor() public{
+        //Constructor functions are functions which run only once when the contract is called
+        // think of it as a initialization function
+        count = 0;
+
+    }
+
+    function getCount() public view returns(uint){
+        return count;
+    }
+    function setcount() public {
+        count += 1;
+    }
+}
+
+
